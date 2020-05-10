fcfbd624f74fd4fe776799f5689706a275381b3d
nazhussain <nazhussain@expedia.com>
2020-05-10 20:17:01 +0530

    Initial setup

---
+++ src/components/student/admission.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default class Admission extends Component {
+  render() {
+    return <Typography paragraph>Student Admission</Typography>;
+  }
+}

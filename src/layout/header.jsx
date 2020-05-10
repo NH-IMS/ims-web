ad11ad46a4f20ad5cb9b324de27f6bd5ae5c03d0
nazhussain <nazhussain@expedia.com>
2020-05-10 17:34:25 +0530

    Initial setup

---
+++ src/layout/header.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import Auth from '../Auth';
+
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Grid,
+} from '@material-ui/core';
+import {
+  ExitToApp,
+  KeyboardBackspaceRounded,
+  MenuRounded,
+} from '@material-ui/icons';
+
+import '../styles/Header.scss';
+
+class Header extends Component {
+  render() {
+    const { open, onOpen, onClose } = this.props;
+    return (
+      <div className='site-header-container'>
+        <AppBar position='fixed'>
+          <Toolbar className='toolbar' variant='dense'>
+            <Grid
+              container
+              direction='row'
+              justify='space-around'
+              alignItems='center'
+            >
+              <Grid item>
+                {open ? (
+                  <IconButton
+                    className='menu-button'
+                    color='inherit'
+                    edge='start'
+                    onClick={onClose}
+                  >
+                    <KeyboardBackspaceRounded />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    className='menu-button'
+                    color='inherit'
+                    edge='start'
+                    onClick={onOpen}
+                  >
+                    <MenuRounded />
+                  </IconButton>
+                )}
+              </Grid>
+
+              <Grid item md={7} xs={3}>
+                <Typography className='title' variant='h5'>
+                  <Link to='/'>Institute Management System</Link>
+                </Typography>
+                <Typography className='titleSmall' variant='h5'>
+                  <Link to='/'>IMS</Link>
+                </Typography>
+              </Grid>
+
+              <Grid
+                item
+                md={1}
+                xs={3}
+                className='account-notification-container'
+              >
+                <Grid
+                  container
+                  direction='row'
+                  justify='space-evenly'
+                  alignItems='center'
+                >
+                  <Grid item>
+                    <IconButton
+                      aria-label='Sign Out'
+                      onClick={() => {
+                        Auth.logout(() => {
+                          this.props.history.push('/');
+                        });
+                      }}
+                      color='inherit'
+                      tooltip={'Sign Out'}
+                    >
+                      <ExitToApp />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Header);
---
+++ src/server/controllers/LoginController.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch');
+const config = require('../config/config.js');
+
+const loginApiConfig = {
+  endPoint: config.endpoints.login,
+};
+
+const LoginController = {
+  async validateSignIn(userName, userPass) {
+    const url = `${loginApiConfig.endPoint}`;
+
+    const reqBody = {
+      userName: `${userName}`,
+      userPass: `${userPass}`,
+    };
+
+    const OPTIONS = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reqBody),
+    };
+    let finalResult = 'error';
+    try {
+      const result = await fetch(url, OPTIONS);
+      finalResult = await result.json();
+      // console.log('finalResult : ' + JSON.stringify(finalResult));
+    } catch (error) {
+      console.log('error in authentication : ' + error);
+    }
+
+    return finalResult;
+  },
+};
+
+module.exports = LoginController;
